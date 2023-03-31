c5e521fb371b1990f34e52501d01461064ebd72b
Woonggyu Jin <wj61@duke.edu>
2023-03-31 16:13:16 -0400

    gameplay frontend abstract classes

---
+++ doc/plan/api/gameplay_frontend/GameNPC/GameNPC.java
@@ -0,0 +1,11 @@
+package gameplay_frontend.GameNPC;
+
+import javafx.scene.image.ImageView;
+
+/**
+ Subclasses for monopoly: Banker, jailer, auction master
+ */
+
+public abstract class GameNPC extends ImageView {
+
+}

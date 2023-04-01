6a743b307c59d98aa533902fead2e9430e69ac5c
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2023-04-01 02:23:44 -0400

    added basic Engine API

---
+++ doc/plan/api/gameplay_backend/Engine.java
@@ -0,0 +1,18 @@
+/***
+ * Thisclass serves as the core logic for the game, by
+ * implementing the game rules and mechanics.
+ */
+public class Engine {
+  ActionsQueue actionsQueue;
+
+  /***
+   * This method is responsible for implementing the game loop and
+   * core game logic by executing actions from rules based on
+   * event triggers
+   *
+   * @param rules
+   */
+  void run(Rule[] rules){
+
+  }
+}

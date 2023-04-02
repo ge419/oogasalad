2a70225d375505a6bd2c1ff508e76b2d45d750cd
Dominic Martinez <dom@dominicm.dev>
2023-04-02 14:31:24 -0400

    Fix typo in interface

--- doc/plan/api/gameplay_backend/event_loop/Action.java
+++ doc/plan/api/gameplay_backend/event_loop/Action.java
@@ -2,4 +2,4 @@
  * The primary abstraction for changing the game state.
  * An action is generally an atomic, unconditional operation.
  */
-interface BiAction extends Consumer<EventEmitter, Chooser> { }
+interface Action extends BiConsumer<EventEmitter, Chooser> { }

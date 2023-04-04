5fe1ccb2c937a1d747c375d2a7fdc064fa920095
Jason Fitzpatrick <jason.fitzpatrick@duke.edu>
2023-04-04 15:00:28 -0400

    Main now extends a java app and displays the example builder view

--- src/main/java/oogasalad/Main.java
+++ src/main/java/oogasalad/Main.java
@@ -1,10 +1,14 @@
 package oogasalad;
 
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import oogasalad.view.builder.BuilderView;
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
-public class Main {
+public class Main extends Application {
     /**
      * A method to test (and a joke :).
      */
@@ -12,11 +16,12 @@ public class Main {
         return 0.001;
     }
 
-    /**
-     * Start of the program.
-     */
-    public static void main (String[] args) {
-        Main m = new Main();
-        System.out.println(m.getVersion());
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        new BuilderView();
     }
 }

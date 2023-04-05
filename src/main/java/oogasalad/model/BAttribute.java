5de036ce6b3dff682dff76e5d89bf047524c3267
윤지원 <jiwon.yoon@duke.edu>
2023-04-05 11:13:15 -0400

    Created BAttribute BScheme File with Variables

---
+++ src/main/java/oogasalad/model/BAttribute.java
@@ -0,0 +1,8 @@
+package oogasalad.model;
+
+public class BAttribute {
+
+  private String key;
+  private String value;
+
+}
---
+++ src/main/java/oogasalad/model/BScheme.java
@@ -0,0 +1,10 @@
+package oogasalad.model;
+
+import java.util.List;
+
+public class BScheme {
+  //TODO: replace Strings with predefined records/ enums
+  private String name;
+  private List<BAttribute> attributes;
+
+}

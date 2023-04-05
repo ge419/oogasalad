27c77fa25e251b4a50e6560f361189a38cb76627
Woonggyu Jin <wj61@duke.edu>
2023-04-04 22:32:18 -0400

    rendering board on view

--- src/main/java/oogasalad/gameplay_frontend/Board.java
+++ src/main/java/oogasalad/gameplay_frontend/Board.java
@@ -1,2 +1,21 @@
-package oogasalad.gameplay_frontend;public class Board {
+package oogasalad.gameplay_frontend;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class Board{
+
+  //Read in from property file
+  private final int BOARD_SIZE = 700;
+  public Board() {
+  }
+
+  public Canvas render() {
+    Canvas boardCanvas = new Canvas(BOARD_SIZE, BOARD_SIZE);
+    GraphicsContext gc = boardCanvas.getGraphicsContext2D();
+    gc.setFill(Color.BLUE);
+    gc.fillRect(0, 0, BOARD_SIZE, BOARD_SIZE);
+    return boardCanvas;
+  }
 }
--- src/main/java/oogasalad/gameplay_frontend/Gameview.java
+++ src/main/java/oogasalad/gameplay_frontend/Gameview.java
@@ -1,2 +1,35 @@
-package oogasalad.gameplay_frontend;public class Gameview {
+package oogasalad.gameplay_frontend;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Gameview extends Application {
+
+  private final int VIEW_WIDTH = 1200;
+  private final int VIEW_HEIGHT = 800;
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+
+    BorderPane UIroot = new BorderPane();
+
+    Board gameboard = new Board();
+
+    UIroot.setCenter(gameboard.render());
+
+    Scene scene = new Scene(UIroot);
+    primaryStage.setTitle("Monopoly");
+    primaryStage.setScene(scene);
+    primaryStage.setHeight(VIEW_HEIGHT);
+    primaryStage.setWidth(VIEW_WIDTH);
+    primaryStage.show();
+  }
 }

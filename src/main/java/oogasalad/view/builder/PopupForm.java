3ac5471f535ad4042470d1736d8c25fbd47784f4
Jason Fitzpatrick <jason.fitzpatrick@duke.edu>
2023-04-06 14:56:36 -0400

    Created the popupform interface and added initial code

---
+++ src/main/java/oogasalad/view/builder/PopupForm.java
@@ -0,0 +1,52 @@
+package oogasalad.view.builder;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface PopupForm<T> {
+    void displayForm(T obj);
+    boolean validateInput();
+    void saveInputToObject(T obj);
+
+    default List<Field> getObjectFields(T obj) {
+        Class<?> clazz = obj.getClass();
+        return Arrays.asList(clazz.getDeclaredFields());
+    }
+
+    default Map<String, Object> getFieldValues(T obj) {
+        Map<String, Object> fieldMap = new HashMap<>();
+        List<Field> fields = getObjectFields(obj);
+
+        for (Field field : fields) {
+            try {
+                field.setAccessible(true);
+                Object value = field.get(obj);
+                fieldMap.put(field.getName(), value);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return fieldMap;
+    }
+
+    default void setFieldValues(T obj, Map<String, Object> fieldMap) {
+        List<Field> fields = getObjectFields(obj);
+
+        for (Field field : fields) {
+            if (fieldMap.containsKey(field.getName())) {
+                try {
+                    field.setAccessible(true);
+                    Object value = fieldMap.get(field.getName());
+                    field.set(obj, value);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    // additional default methods will exist to set id, create the javafx, etc
+}

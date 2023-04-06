c9cc51d2fa3fc0b5deef9644ebd2151c97b188c0
Woonggyu Jin <wj61@duke.edu>
2023-04-06 12:56:04 -0400

    boilerplate testing code

---
+++ src/test/java/oogasalad/gameplay_frontend/GameviewTest.java
@@ -0,0 +1,30 @@
+package oogasalad.gameplay_frontend;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxAssert;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+
+public class GameviewTest extends ApplicationTest {
+
+  @Override
+  public void start(Stage primaryStage) {
+    // create a scene for testing
+    Scene scene = new Scene(/* add the root node for the scene here */);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  @Test
+  public void testRenderGameview() {
+    // call the method to be tested
+    new Gameview().renderGameview(stage);
+
+    // perform assertions on the UI elements
+    FxAssert.verifyThat(/* add a selector for a UI element to be tested */, NodeMatchers.isVisible());
+    // add additional assertions as needed
+  }
+
+}

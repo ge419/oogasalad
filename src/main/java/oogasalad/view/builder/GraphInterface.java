95282ac396affb9c8d99465aa482e6a47d4444e8
Trevon Helm <trevon.helm@duke.edu>
2023-04-06 00:31:47 -0400

    Implemented the Graph class.

--- src/main/java/oogasalad/view/builder/GraphInterface.java
+++ src/main/java/oogasalad/view/builder/GraphInterface.java
@@ -1,7 +1,12 @@
 package oogasalad.view.builder;
 
+import java.util.List;
+
 public interface GraphInterface {
+    void addTile(Tile tile);
     void addTileNext(Tile tile, Tile nextTile);
-    void removeNextTileFrom(Tile tile, Tile nextTile);
-    void numberOfNextTiles(Tile desiredTile);
+    void removeNextTile(Tile tile, Tile nextTile);
+    int numberOfNextTiles(Tile desiredTile);
+    List<Tile> getNextTiles(Tile desiredTile);
+    List<Tile> getTiles();
 }

b3fa46e4760a0cab40f5cedcc9dea0fb5eb8a6c0
Changmin Shin <cs521@duke.edu>
2023-04-07 19:56:47 -0400

    added basic structure of SaveDirectory

--- src/main/java/oogasalad/model/builder/SaveDirectory.java
+++ src/main/java/oogasalad/model/builder/SaveDirectory.java
@@ -1,12 +1,34 @@
 package oogasalad.model.builder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+
 public class SaveDirectory implements SaveGame{
 
+//  private final String title;
+//  private final ? tiles;
+//  private final ? rules;
+//  private final ? players;
+//  private final ? ...
+
+  /**
+   * takes DataStorer datastorer as parameter
+   */
   public SaveDirectory() {
+//    this.title = datastoere.title();
+//    this.tiles = datastorer.tiles();
+//    this.rules = datastorer.rules();
+//    this.players = datastorer.players();
+//    this... = datastorer...
   }
 
+  /**
+   * save using serializer
+   */
   @Override
-  public void save() {
-
+  public void save(Object o) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.writeValue(new File("data/title/*.json"), o);
   }
 }

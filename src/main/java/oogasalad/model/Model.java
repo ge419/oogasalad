7d7898295d461e1ee7deebab534b9fcab04d143c
윤지원 <jiwon.yoon@duke.edu>
2023-04-07 14:16:54 -0400

    Tested Working Constructable Factory with BAttributes and BSchema

--- src/main/java/oogasalad/model/Model.java
+++ src/main/java/oogasalad/model/Model.java
@@ -7,14 +7,22 @@ import java.io.IOException;
 public class Model {
   public static void main(String[] args) throws IOException {
     BFactory factory = new BFactory();
-    File file = new File("data/example/ExamplePlayer.json");
-    Constructable b = factory.generate(file);
-    System.out.println(b.getAttributeValue("self"));
-    System.out.println(b.getAttributeValue("id"));
-    System.out.println(b.getAttributeValue("name"));
-    System.out.println(b.getAttributeValue("score"));
-    b.updateAttributeValue("score", new BValue(1000));
-    System.out.println(b.getAttributeValue("score"));
+    File playerfile = new File("data/example/ExamplePlayer.json");
+    Constructable player = factory.generate(playerfile);
+
+    File tilefile = new File("data/example/ExampleTile.json");
+    Constructable tile = factory.generate(tilefile);
+
+    System.out.println(player.getAttributeValue("self"));
+    System.out.println(player.getAttributeValue("id"));
+    System.out.println(player.getAttributeValue("name"));
+    System.out.println(player.getAttributeValue("score"));
+    player.updateAttributeValue("score", new BValue(1000));
+    System.out.println(player.getAttributeValue("score"));
+
+    System.out.println(tile.getAttributeValue("self"));
+    System.out.println(tile.getAttributeValue("xPosition"));
+    System.out.println(tile.getAttributeValue("yPosition"));
   }
 
 }

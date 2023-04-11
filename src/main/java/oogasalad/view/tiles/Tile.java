8a449dbc5d48a6ec0a62503a08be80ed0d51fd38
Trevon Helm <trevon.helm@duke.edu>
2023-04-11 19:37:28 -0400

    Updated Tile interface as discussed in our meeting to remove unused methods. This interface will be solely for the JavaFX components of a tile, so any future JavaFX manipulations of a Tile will be an API from this interface. Added a getColor method to complement setColor.

--- src/main/java/oogasalad/view/tiles/Tile.java
+++ src/main/java/oogasalad/view/tiles/Tile.java
@@ -1,19 +1,16 @@
 package oogasalad.view.tiles;
 
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import oogasalad.view.Coordinate;
 
 public interface Tile {
   public int getTileId();
 
   public double[] getPosition();
-
-  public int[] getNext();
-
-  public int[] getOnLand();
-
-  public int[] getAfterTurn();
-  public void setColor(Color color);
   public void setPosition(Coordinate coord);
+
+  public Paint getColor();
+  public void setColor(Color color);
 
 }

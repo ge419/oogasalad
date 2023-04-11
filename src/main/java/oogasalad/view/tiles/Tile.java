286406feec1e0441cd376f1d956080f0005de1c4
Trevon Helm <trevon.helm@duke.edu>
2023-04-11 19:39:17 -0400

    forgot to update tile for google style

--- src/main/java/oogasalad/view/tiles/Tile.java
+++ src/main/java/oogasalad/view/tiles/Tile.java
@@ -5,12 +5,15 @@ import javafx.scene.paint.Paint;
 import oogasalad.view.Coordinate;
 
 public interface Tile {
+
   public int getTileId();
 
   public double[] getPosition();
+
   public void setPosition(Coordinate coord);
 
   public Paint getColor();
+
   public void setColor(Color color);
 
 }

4020cf509d4a4303cdf3b3d9d3b731e2ef17a676
윤지원 <jiwon.yoon@duke.edu>
2023-04-12 21:21:07 -0400

    Automation of Next Tile Setting

--- src/main/java/oogasalad/model/attribute/Model.java
+++ src/main/java/oogasalad/model/attribute/Model.java
@@ -16,6 +16,7 @@ import oogasalad.model.constructable.Tile;
 
 
 public class Model {
+  public static ArrayList<String> nextIds = new ArrayList<>();
 
   public static void main(String[] args)
       throws IOException {
@@ -46,6 +47,10 @@ public class Model {
       x+=50;
       tiles.add(t);
     }
+    for (int i=0; i<tiles.size()-1; i++) {
+      tiles.get(i).getNextTileIds().add(tiles.get(i+1).getId());
+    }
+    tiles.get(7).getNextTileIds().add(tiles.get(0).getId());
     BBoard board = new BBoard(tiles);
     objectMapper.writeValue(new File("data/tiles.json"), board);
 

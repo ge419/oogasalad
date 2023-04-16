38ed1204d3b34deb5fbdc50bb3be39f5da670fb6
Nathaniel Wullar <nathaniel.wullar@duke.edu>
2023-04-16 16:58:08 -0400

    remove repeated setCards method

--- src/main/java/oogasalad/view/builder/gameholder/GameHolderBuilder.java
+++ src/main/java/oogasalad/view/builder/gameholder/GameHolderBuilder.java
@@ -11,19 +11,14 @@ public class GameHolderBuilder {
   Optional<List<Player>> players;
   // Optional<List<Card>> cards;
 
-  public GameHolderBuilder setPlayers(Optional<List<Player>> players) {
-    this.players = players;
+  public GameHolderBuilder setPlayers(List<Player> players) {
+    this.players = Optional.of(players);
     return this;
   }
 
 //  public void setCards(List<T> cards) {
 //    this.cards = Optional.of(cards);
 //  }
-
-  public GameHolderBuilder setCards(List<Player> players){
-    this.players = Optional.of(players);
-    return this;
-  }
 
   public GameHolderBuilder setBoard(BBoard board) {
     this.board = board;

7f8a4c8aa195fc70787892e8cbf4e5bd1ce44ac2
Jason Fitzpatrick <jason.fitzpatrick@duke.edu>
2023-04-17 16:15:51 -0400

    Added additional tests for getAttribute and getMetadata for BooleanParameterStrategy

--- src/test/java/oogasalad/view/builder/popupform/BooleanParameterStrategyTest.java
+++ src/test/java/oogasalad/view/builder/popupform/BooleanParameterStrategyTest.java
@@ -18,14 +18,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class BooleanParameterStrategyTest extends DukeApplicationTest {
     private static final String BASE_RESOURCE_PACKAGE = "view.builder.";
     private BooleanParameterStrategy booleanParameterStrategy;
-    private Metadata metadata;
+    private BooleanMetadata meta;
+    private BooleanAttribute attr;
     @Override
     public void start(Stage stage) {
         ResourceBundle resourceBundle = ResourceBundle.getBundle(BASE_RESOURCE_PACKAGE + "EnglishBuilderText");
         String attributeKey = "attr";
-        BooleanMetadata meta = new BooleanMetadata(attributeKey);
+        meta = new BooleanMetadata(attributeKey);
         meta.setName(attributeKey);
-        BooleanAttribute attr = meta.makeBooleanAttribute();
+        attr = meta.makeBooleanAttribute();
         booleanParameterStrategy = new BooleanParameterStrategy(attr, meta);
         VBox root = new VBox(booleanParameterStrategy.renderInput(resourceBundle));
         Scene scene = new Scene(root);
@@ -42,7 +43,7 @@ class BooleanParameterStrategyTest extends DukeApplicationTest {
     }
 
     @Test
-    void validateInput() {
+    void isInputValid() {
         CheckBox field = lookup("#attr").query();
         clickOn(field);
         assertEquals(true, booleanParameterStrategy.isInputValid());
@@ -50,18 +51,8 @@ class BooleanParameterStrategyTest extends DukeApplicationTest {
         clickOn(field);
         assertEquals(true, booleanParameterStrategy.isInputValid());
     }
-
     @Test
-    void getValue() {
-        CheckBox field = lookup("#attr").query();
-        clickOn(field);
-        assertEquals(true, field.isSelected());
-        clickOn(field);
-        assertEquals(false, field.isSelected());
-    }
-
-    @Test
-    void setValue() {
+    void saveInput() {
         CheckBox field = lookup("#attr").query();
         clickOn(field);
         BooleanAttribute booleanAttr = (BooleanAttribute) booleanParameterStrategy.getAttribute();
@@ -70,5 +61,16 @@ class BooleanParameterStrategyTest extends DukeApplicationTest {
         clickOn(field);
         booleanParameterStrategy.saveInput();
         assertEquals(false, booleanAttr.getValue());
+    }
+    @Test
+    void getMetadata() {
+        BooleanMetadata booleanMeta = (BooleanMetadata) booleanParameterStrategy.getMetadata();
+        assertEquals(booleanMeta.getKey(), meta.getKey());
+        assertEquals(booleanMeta.getName(), meta.getName());
+    }
+    @Test
+    void getAttribute() {
+        BooleanAttribute booleanAttr = (BooleanAttribute) booleanParameterStrategy.getAttribute();
+        assertEquals(booleanAttr.getKey(), attr.getKey());
     }
 }

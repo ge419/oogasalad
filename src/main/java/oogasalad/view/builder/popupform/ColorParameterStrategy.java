bdc020672690400a9a2c5f6efb65b87563edddbf
Jason Fitzpatrick <jason.fitzpatrick@duke.edu>
2023-04-17 15:10:13 -0400

    Refactored colorparameterstrategy. Color attribute does not yet exist, so this is commented out for now

--- src/main/java/oogasalad/view/builder/popupform/ColorParameterStrategy.java
+++ src/main/java/oogasalad/view/builder/popupform/ColorParameterStrategy.java
@@ -1,39 +1,62 @@
 package oogasalad.view.builder.popupform;
 
 import java.util.ResourceBundle;
+
+import com.google.inject.assistedinject.Assisted;
 import javafx.scene.Node;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import oogasalad.model.attribute.Attribute;
+import oogasalad.model.attribute.BooleanAttribute;
+import oogasalad.model.attribute.BooleanMetadata;
 import oogasalad.model.attribute.Metadata;
 import oogasalad.view.builder.BuilderUtility;
 
-class ColorParameterStrategy implements ParameterStrategy, BuilderUtility {
-    private ColorPicker element = new ColorPicker();
+import javax.inject.Inject;
 
-    public ColorParameterStrategy() {
-    }
-
-    @Override
-    public Node renderInput(String name, ResourceBundle resourceBundle) {
-        Node textLabel = new Text(name);
-        element = (ColorPicker) makeColorPicker(name);
-        return makeHBox(String.format("%sColorInput", name), textLabel, element);
-    }
-
-    @Override
-    public Color getValue() {
-        return element.getValue();
-    }
-
-    @Override
-    public boolean validateInput(Metadata metadata) {
-        return getValue().getClass().equals(Color.class);
-    }
-
-    @Override
-    public void setValue(Attribute attribute) {
-        //ColorAttribute.from(attribute).setValue(getValue());
-    }
-}
+//class ColorParameterStrategy implements ParameterStrategy, BuilderUtility {
+//    private final ColorAttribute attr;
+//    private final ColorMetadata meta;
+//    private ColorPicker element = new ColorPicker();
+//
+//    @Inject
+//    public ColorParameterStrategy(
+//            @Assisted Attribute attr,
+//            @Assisted Metadata meta) {
+//        this.attr = ColorAttribute.from(attr);
+//        this.meta = ColorMetadata.from(meta);
+//    }
+//
+//    @Override
+//    public Node renderInput(ResourceBundle resourceBundle) {
+//        String name = meta.getName();
+//        Node textLabel = new Text(name);
+//        element = (ColorPicker) makeColorPicker(name);
+//        return makeHBox(String.format("%sColorInput", name), textLabel, element);
+//    }
+//
+//    @Override
+//    public void saveInput() {
+//        attr.setValue(getFieldValue());
+//    }
+//
+//    @Override
+//    public boolean isInputValid() {
+//        return meta.isValidValue(getFieldValue());
+//    }
+//
+//    @Override
+//    public Metadata getMetadata() {
+//        return meta;
+//    }
+//
+//    @Override
+//    public Attribute getAttribute() {
+//        return attr;
+//    }
+//
+//    private Color getFieldValue() {
+//        return element.getValue();
+//    }
+//}

2f1c1c4b8ba1ba2a6b3f5800d00d85d3e6b1e9cd
Jason Fitzpatrick <jason.fitzpatrick@duke.edu>
2023-04-17 15:09:24 -0400

    Refactored BooleanParameterStrategy to match structure of Parameter strategy interface

--- src/main/java/oogasalad/view/builder/popupform/BooleanParameterStrategy.java
+++ src/main/java/oogasalad/view/builder/popupform/BooleanParameterStrategy.java
@@ -1,32 +1,54 @@
 package oogasalad.view.builder.popupform;
 
+import com.google.inject.assistedinject.Assisted;
 import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
 import javafx.scene.text.Text;
 import oogasalad.model.attribute.*;
 import oogasalad.view.builder.BuilderUtility;
 
+import javax.inject.Inject;
 import java.util.ResourceBundle;
 
 class BooleanParameterStrategy implements ParameterStrategy, BuilderUtility {
+    private final BooleanAttribute attr;
+    private final BooleanMetadata meta;
     private CheckBox element;
-    public BooleanParameterStrategy(){}
+    @Inject
+    public BooleanParameterStrategy(
+            @Assisted Attribute attr,
+            @Assisted Metadata meta) {
+        this.attr = BooleanAttribute.from(attr);
+        this.meta = BooleanMetadata.from(meta);
+    }
     @Override
-    public Node renderInput(String name, ResourceBundle resourceBundle) {
+    public Node renderInput(ResourceBundle resourceBundle) {
+        String name = meta.getName();
         Node textLabel = new Text(name+" (True/False)");
         element = (CheckBox) makeCheckBox(name);
         return makeHBox(String.format("%sBooleanInput", name), textLabel, element);
     }
     @Override
-    public boolean validateInput(Metadata metadata) {
-        return getValue().getClass().equals(Boolean.class);
+    public void saveInput() {
+        attr.setValue(getFieldValue());
     }
+
     @Override
-    public Boolean getValue() {
+    public boolean isInputValid() {
+        return meta.isValidValue(getFieldValue());
+    }
+
+    @Override
+    public Metadata getMetadata() {
+        return meta;
+    }
+
+    @Override
+    public Attribute getAttribute() {
+        return attr;
+    }
+
+    private boolean getFieldValue() {
         return element.isSelected();
-    }
-    @Override
-    public void setValue(Attribute attribute) {
-        BooleanAttribute.from(attribute).setValue(getValue());
     }
 }

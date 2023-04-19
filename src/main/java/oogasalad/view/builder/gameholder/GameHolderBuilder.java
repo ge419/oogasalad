534c48f4b8a1bd1bbba116940c94df2d253cdcd3
Changmin Shin <cs521@duke.edu>
2023-04-19 18:09:13 -0400

    add gameobjectype to gameholderbuilder

--- src/main/java/oogasalad/view/builder/gameholder/GameHolderBuilder.java
+++ src/main/java/oogasalad/view/builder/gameholder/GameHolderBuilder.java
@@ -9,23 +9,33 @@ import oogasalad.view.builder.board.ImmutableBoardInfo;
 public class GameHolderBuilder {
   BBoard board;
   Optional<List<Player>> players;
-  // Optional<List<Card>> cards;
+  GameObjectType gameObjectType;
 
+  // Optional<List<Card>> cards;
+  public GameHolderBuilder(){}
   public GameHolderBuilder setPlayers(List<Player> players) {
     this.players = Optional.of(players);
+    this.gameObjectType = GameObjectType.PLAYER;
     return this;
   }
 
 //  public void setCards(List<T> cards) {
 //    this.cards = Optional.of(cards);
+//    this.gameObjectType = GameObjectType.CARD;
 //  }
 
   public GameHolderBuilder setBoard(BBoard board) {
     this.board = board;
+    this.gameObjectType = GameObjectType.BOARD;
     return this;
   }
+
+//  public String setThing()
   public GameHolder getGameHolder(){
     return new GameHolder();
   }
+//  List<Player> thing  = null;
+//  GameHolder a  = new GameHolderBuilder().setPlayers(thing)..setgetGameHolder();
+
 
 }

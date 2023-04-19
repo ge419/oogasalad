448a24aa8263b83f0e5145b850b5431fd3d0ae38
Changmin Shin <cs521@duke.edu>
2023-04-19 18:24:58 -0400

    create GameHolderDirector

--- src/main/java/oogasalad/view/builder/gameholder/GameHolder.java
+++ src/main/java/oogasalad/view/builder/gameholder/GameHolder.java
@@ -8,48 +8,47 @@ import oogasalad.view.builder.board.ImmutableBoardInfo;
 import oogasalad.view.builder.graphs.ImmutableGraph;
 
 
-public class GameHolder implements GameInterface, MutableGameInterface {
+public class GameHolder {
 
-  ImmutableGraph myGraph;
-  ImmutableBoardInfo myBoardInfo;
+//  ImmutableGraph myGraph;
+//  ImmutableBoardInfo myBoardInfo;
   BBoard board;
   Optional<List<Player>> players;
   // Optional<List<Card>> cards;
 
   public GameHolder(ImmutableGraph myGraph, ImmutableBoardInfo myBoardInfo, BBoard board,
       Optional<List<Player>> players) {
-    this.myGraph = myGraph;
-    this.myBoardInfo = myBoardInfo;
+//    this.myGraph = myGraph;
+//    this.myBoardInfo = myBoardInfo;
     this.board = board;
     this.players = players;
   }
 
   public GameHolder() {
-    //myGraph = new Graph();
   }
 
-  @Override
-  public ImmutableGraph getTileGraph() {
-    return myGraph;
-  }
-  @Override
-  public void setTileGraph(ImmutableGraph graph) {
-    myGraph = graph;
-  }
-
-  @Override
-  public ImmutableBoardInfo getBoardInfo() {
-    return myBoardInfo;
-  }
-
-  @Override
-  public void setBoardInfo(ImmutableBoardInfo board) {
-    myBoardInfo = board;
-  }
-
-  public ImmutableBoardInfo getMyBoardInfo() {
-    return myBoardInfo;
-  }
+//  @Override
+//  public ImmutableGraph getTileGraph() {
+//    return myGraph;
+//  }
+//  @Override
+//  public void setTileGraph(ImmutableGraph graph) {
+//    myGraph = graph;
+//  }
+//
+//  @Override
+//  public ImmutableBoardInfo getBoardInfo() {
+//    return myBoardInfo;
+//  }
+//
+//  @Override
+//  public void setBoardInfo(ImmutableBoardInfo board) {
+//    myBoardInfo = board;
+//  }
+//
+//  public ImmutableBoardInfo getMyBoardInfo() {
+//    return myBoardInfo;
+//  }
   public BBoard getBoard() {
     return board;
   }

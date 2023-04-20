db29c003073184f5f0d213d7f6c624143154cdbf
Aloye Oshotse <ajo24@duke.edu>
2023-04-20 09:31:58 -0400

    rewrote GamePiece to extend StackPane instead of ImageView

--- src/main/java/oogasalad/view/gameplay/pieces/GamePiece.java
+++ src/main/java/oogasalad/view/gameplay/pieces/GamePiece.java
@@ -2,24 +2,31 @@ package oogasalad.view.gameplay.pieces;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import oogasalad.view.Imageable;
 import oogasalad.view.gameplay.Movable;
 
-abstract public class GamePiece extends ImageView implements Movable {
+abstract public class GamePiece extends StackPane implements Movable, Imageable {
 
   private static final double PIECE_SIZE = 40;
-  protected Image image;
+
+  private Image image;
   private double xCoor;
   private double yCoor;
+  private ImageView imageView;
 
   public GamePiece(String imageURL) {
 
     try {
       image = new Image(new FileInputStream(imageURL));
-      this.setImage(image);
-      this.setFitHeight(PIECE_SIZE);
-      this.setFitWidth(PIECE_SIZE);
+      imageView = new ImageView(image);
+      imageView.setFitHeight(PIECE_SIZE);
+      imageView.setFitWidth(PIECE_SIZE);
+      getChildren().add(imageView);
+
     } catch (IOException e) {
       e.printStackTrace();
     }

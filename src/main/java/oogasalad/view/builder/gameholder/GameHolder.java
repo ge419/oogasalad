303ba862a7cec86dbdab133270e6e5c98a496936
Changmin Shin <cs521@duke.edu>
2023-04-20 16:17:09 -0400

    initial version of GameHolder/Builder complete

--- src/main/java/oogasalad/view/builder/gameholder/GameHolder.java
+++ src/main/java/oogasalad/view/builder/gameholder/GameHolder.java
@@ -10,55 +10,20 @@ import oogasalad.view.builder.graphs.ImmutableGraph;
 
 public class GameHolder {
 
-//  ImmutableGraph myGraph;
-//  ImmutableBoardInfo myBoardInfo;
   BBoard board;
   Optional<List<Player>> players;
   // Optional<List<Card>> cards;
 
-  //TODO: fix parameters (remove immutable classes, make generic)
-  public GameHolder(ImmutableGraph myGraph, ImmutableBoardInfo myBoardInfo, BBoard board,
-      Optional<List<Player>> players) {
-//    this.myGraph = myGraph;
-//    this.myBoardInfo = myBoardInfo;
-    this.board = board;
-    this.players = players;
-//    this.cards = cards
-  }
-
-  public GameHolder(GameHolderBuilder builder) {
+  public GameHolder() {
 
   }
 
-//  @Override
-//  public ImmutableGraph getTileGraph() {
-//    return myGraph;
-//  }
-//  @Override
-//  public void setTileGraph(ImmutableGraph graph) {
-//    myGraph = graph;
-//  }
-//
-//  @Override
-//  public ImmutableBoardInfo getBoardInfo() {
-//    return myBoardInfo;
-//  }
-//
-//  @Override
-//  public void setBoardInfo(ImmutableBoardInfo board) {
-//    myBoardInfo = board;
-//  }
-//
-//  public ImmutableBoardInfo getMyBoardInfo() {
-//    return myBoardInfo;
-//  }
   public BBoard getBoard() {
     return board;
   }
   public Optional<List<Player>> getPlayers() {
     return players;
   }
-
 //  public Optional<List<Card>> getCards() {
 //    return cards;
 //  }
--- src/main/java/oogasalad/view/builder/gameholder/GameHolderBuilder.java
+++ src/main/java/oogasalad/view/builder/gameholder/GameHolderBuilder.java
@@ -11,100 +11,20 @@ import oogasalad.model.constructable.BBoard;
 import oogasalad.model.constructable.Player;
 import oogasalad.view.builder.board.ImmutableBoardInfo;
 
-/**
- * https://howtocodetutorial.wordpress.com/generic-builder-pattern-in-java-8/
- * https://www.sayem.org/generic-builder-pattern/
- * @param <T>
- */
-
-public class GameHolderBuilder<T> {
-  private T instance;
-  private boolean ifCond = true; // default
-  public GameHolderBuilder(Class<T> clazz) {
-    try {
-      instance = clazz.newInstance();
-    } catch (InstantiationException | IllegalAccessException e) {
-      e.printStackTrace();
-    }
-  }
-//public class GameHolderBuilder<T extends GameHolder> {
-//  private T instance;
-//
-//  public GameHolderBuilder(Class<T> clazz) {
-//
-//  }
+public class GameHolderBuilder {
 
   BBoard board;
   Optional<List<Player>> players;
   GameObjectType gameObjectType;
+//   Optional<List<Card>> cards;
 
-  // Optional<List<Card>> cards;
-
-  public GameHolderBuilder(){
-
-  }
-
-  public GameHolderBuilder<T> with(Consumer<T> setter) {
-    if (ifCond) setter.accept(instance);
-    return this;
-  }
-
-  public T get(){
-    return instance;
-  }
-
-  public static <T> GameHolderBuilder<T> build(Class<T> clazz) {
-    return new GameHolderBuilder<>(clazz);
-  }
-
-  public GameHolderBuilder<T> If(BooleanSupplier condition){
-    this.ifCond = condition.getAsBoolean();
-    return this;
-  }
-
-  public GameHolderBuilder<T> endIf(){
-    this.ifCond = true;
-    return this;
-  }
-
-
-    private final Supplier<T> instantiator;
-
-    private List<Consumer<T>> instanceModifiers = new ArrayList<>();
-
-    public GameHolderBuilder(Supplier<T> instantiator) {
-      this.instantiator = instantiator;
-    }
-
-    public static <T> GameHolderBuilder<T> of(Supplier<T> instantiator) {
-      return new GameHolderBuilder<T>(instantiator);
-    }
-
-    public <U> GameHolderBuilder<T> with(BiConsumer<T, U> consumer, U value) {
-      Consumer<T> c = instance -> consumer.accept(instance, value);
-      instanceModifiers.add(c);
-      return this;
-    }
-
-    public T build() {
-      T value = instantiator.get();
-      instanceModifiers.forEach(modifier -> modifier.accept(value));
-      instanceModifiers.clear();
-      return value;
-    }
-
-
-
-
-
-
-  public GameHolderBuilder setPlayers(List<Player> players) {
-    this.players = Optional.of(players);
+  public GameHolderBuilder setPlayers(Optional<List<Player>> players) {
+    this.players = players;
     this.gameObjectType = GameObjectType.PLAYER;
     return this;
   }
 
-//  public void setCards(List<T> cards) {
+//  public void setCards(List<Card> cards) {
 //    this.cards = Optional.of(cards);
 //    this.gameObjectType = GameObjectType.CARD;
 //  }
@@ -116,8 +36,97 @@ public class GameHolderBuilder<T> {
   }
 
   public GameHolder build(){
-    return new GameHolder(this);
+    GameHolder holder = new GameHolder();
+    holder.board = this.board;
+    holder.players = this.players;
+//    holder.cards = this.cards;
+
+    GameHolder gameholder = setBoard(board).setPlayers(players).build();
+    return holder;
   }
-
-
 }
+
+
+
+
+///**
+// * https://howtocodetutorial.wordpress.com/generic-builder-pattern-in-java-8/
+// * https://www.sayem.org/generic-builder-pattern/
+// * @param <T>
+// */
+//
+//public class GameHolderBuilder<T> {
+//  private T instance;
+//  private boolean ifCond = true; // default
+//  public GameHolderBuilder(Class<T> clazz) {
+//    try {
+//      instance = clazz.newInstance();
+//    } catch (InstantiationException | IllegalAccessException e) {
+//      e.printStackTrace();
+//    }
+//  }
+////public class GameHolderBuilder<T extends GameHolder> {
+////  private T instance;
+////
+////  public GameHolderBuilder(Class<T> clazz) {
+////
+////  }
+//
+//  BBoard board;
+//  Optional<List<Player>> players;
+//  GameObjectType gameObjectType;
+//
+//  // Optional<List<Card>> cards;
+//
+//  public GameHolderBuilder(){
+//
+//  }
+//
+//  public GameHolderBuilder<T> with(Consumer<T> setter) {
+//    if (ifCond) setter.accept(instance);
+//    return this;
+//  }
+//
+//  public T get(){
+//    return instance;
+//  }
+//
+//  public static <T> GameHolderBuilder<T> build(Class<T> clazz) {
+//    return new GameHolderBuilder<>(clazz);
+//  }
+//
+//  public GameHolderBuilder<T> If(BooleanSupplier condition){
+//    this.ifCond = condition.getAsBoolean();
+//    return this;
+//  }
+//
+//  public GameHolderBuilder<T> endIf(){
+//    this.ifCond = true;
+//    return this;
+//  }
+//
+//
+//  private final Supplier<T> instantiator;
+//
+//  private List<Consumer<T>> instanceModifiers = new ArrayList<>();
+//
+//  public GameHolderBuilder(Supplier<T> instantiator) {
+//    this.instantiator = instantiator;
+//  }
+//
+//  public static <T> GameHolderBuilder<T> of(Supplier<T> instantiator) {
+//    return new GameHolderBuilder<T>(instantiator);
+//  }
+//
+//  public <U> GameHolderBuilder<T> with(BiConsumer<T, U> consumer, U value) {
+//    Consumer<T> c = instance -> consumer.accept(instance, value);
+//    instanceModifiers.add(c);
+//    return this;
+//  }
+//
+//  public T build() {
+//    T value = instantiator.get();
+//    instanceModifiers.forEach(modifier -> modifier.accept(value));
+//    instanceModifiers.clear();
+//    return value;
+//  }

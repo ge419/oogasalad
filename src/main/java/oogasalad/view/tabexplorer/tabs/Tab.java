104d6572e397c353fe9c41e54ffff6f248db1014
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2023-04-22 02:33:42 -0400

    added method/javadoc to `Tab` interface

--- src/main/java/oogasalad/view/tabexplorer/tabs/Tab.java
+++ src/main/java/oogasalad/view/tabexplorer/tabs/Tab.java
@@ -1,5 +1,15 @@
 package oogasalad.view.tabexplorer.tabs;
 
+/**
+ * This interface represents a tab in the application, which can be displayed in a tabbed interface.
+ * Any class that implements this interface must implement the {@code renderTabContent()} method,
+ * which will be called to render the content of the tab.
+ */
 public interface Tab {
 
+  /**
+   * This method is called to render the content of the tab. Any implementing class should use this
+   * method to create and add the necessary UI components to display the content of the tab.
+   */
+  void renderTabContent();
 }

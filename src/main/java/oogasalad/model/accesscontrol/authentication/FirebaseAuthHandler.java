78c40f56f50e185810a916fa9dc5d5929bcd1619
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2023-04-22 15:01:58 -0400

    added initial impl for firebase auth handling

--- src/main/java/oogasalad/model/accesscontrol/authentication/FirebaseAuthHandler.java
+++ src/main/java/oogasalad/model/accesscontrol/authentication/FirebaseAuthHandler.java
@@ -1,5 +1,49 @@
 package oogasalad.model.accesscontrol.authentication;
 
-public class FirebaseAuthHandler implements AuthenticationHandler {
+import com.google.inject.Inject;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import oogasalad.model.accesscontrol.database.DatabaseAccessor;
 
+public class FirebaseAuthHandler implements AuthenticationHandler {
+  private boolean userLogInStatus = false;
+  private String activeUser = null;
+  private DatabaseAccessor db;
+  @Inject
+  public FirebaseAuthHandler(DatabaseAccessor db){
+    this.db = db;
+  }
+  @Override
+  public void login(String username, String password) {
+    if (!db.isUserRegistered(username)){
+      register( username,  password);
+    }
+    activeUser = username;
+    setUserLogInStatus(true);
+  }
+  @Override
+  public void logout() {
+    setUserLogInStatus(false);
+  }
+  @Override
+  public void register(String username, String password) {
+    Map<String, Object> docData = new HashMap<>();
+    docData.put("username",  username);
+    docData.put("password", password);
+    docData.put("games", Arrays.asList());
+
+    db.modifyUser(docData);
+  }
+  @Override
+  public boolean getUserLogInStatus() {
+    return userLogInStatus;
+  }
+  @Override
+  public String getActiveUser() {
+    return activeUser;
+  }
+  private void setUserLogInStatus(boolean logInStatus) {
+    userLogInStatus = logInStatus;
+  }
 }

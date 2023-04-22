e2a4cb035c3040946a7b631c05b77f6a925b73f5
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2023-04-22 03:01:22 -0400

    added guice module for tab explorer

--- src/main/java/oogasalad/view/tabexplorer/TabModule.java
+++ src/main/java/oogasalad/view/tabexplorer/TabModule.java
@@ -1,5 +1,19 @@
 package oogasalad.view.tabexplorer;
 
-public class TabModule {
+import com.google.inject.AbstractModule;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+import oogasalad.model.accesscontrol.AccessControlModule;
+import oogasalad.view.tabexplorer.tabs.TabFactory;
 
+/**
+ * Guice module for the tab explorer view. It installs the {@link AccessControlModule} and the
+ * {@link TabFactory} and defines bindings for controllers that are used by the tab explorer view.
+ */
+public class TabModule extends AbstractModule {
+  @Override
+  protected void configure() {
+    install(new AccessControlModule());
+    install(new FactoryModuleBuilder().build(TabFactory.class));
+    //todo add binding for controller
+  }
 }

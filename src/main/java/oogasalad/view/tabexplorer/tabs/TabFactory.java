e529932ae1a535e2a269cc2e559b44b3252dc5a0
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2023-04-22 03:32:44 -0400

    added methods to TabFactory interface

--- src/main/java/oogasalad/view/tabexplorer/tabs/TabFactory.java
+++ src/main/java/oogasalad/view/tabexplorer/tabs/TabFactory.java
@@ -1,5 +1,34 @@
 package oogasalad.view.tabexplorer.tabs;
 
+import oogasalad.view.tabexplorer.TabExplorer;
+import oogasalad.view.tabexplorer.tabs.socialcenter.SocialCenterTab;
+
+/**
+ * The TabFactory interface provides methods for creating various tabs in a tab explorer view.
+ *
+ * @author Chika Dueke-Eze
+ */
 public interface TabFactory {
 
+  /**
+   * Creates a LoginTab instance for the specified TabExplorer.
+   * @param tabExplorer the TabExplorer instance for the LoginTab
+   * @return a LoginTab instance
+   */
+  LoginTab makeLoginTab(TabExplorer tabExplorer);
+  /**
+   * Creates a GameLauncherTab instance for the specified TabExplorer.
+   * @param tabExplorer the TabExplorer instance for the GameLauncherTab
+   * @returna GameLauncherTab instance
+   */
+  GameLauncherTab makeGameLauncherTab(TabExplorer tabExplorer);
+  /**
+   * Creates a SocialCenterTab instance for the specified TabExplorer.
+   * @param tabExplorer the TabExplorer instance for the SocialCenterTab
+   * @return a SocialCenterTab instance
+   */
+  SocialCenterTab makeSocialCenterTab(TabExplorer tabExplorer);
 }
+
+
+

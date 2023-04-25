d5f6d53b2ae9eada09dbf85b1058d68d2cc094ac
윤지원 <jiwon.yoon@duke.edu>
2023-04-25 00:28:23 -0400

    Controller Unit Tests

--- src/test/java/oogasalad/controller/GameControllerTest.java
+++ src/test/java/oogasalad/controller/GameControllerTest.java
@@ -2,6 +2,38 @@ package oogasalad.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import oogasalad.model.attribute.SchemaDatabase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 class GameControllerTest {
+  SchemaDatabase db;
+
+  @BeforeEach
+  void setUp() {
+    db = new SchemaDatabase();
+  }
+
+  @Test
+  void loadSchemas() {
+    assertTrue(db.getSchema("basicTile").isPresent());
+    assertEquals(5, db.getAllSchemaNames().size());
+  }
+
+  @Test
+  void loadTilesDeserialization() {
+
+  }
+
+  @Test
+  void loadTilesValidValues() {
+
+  }
+
+  @Test
+  void loadPlayersDeserialization() {
+
+  }
 
 }

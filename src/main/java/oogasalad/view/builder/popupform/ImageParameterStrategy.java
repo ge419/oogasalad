5acd97a4f08bdf151e400620333d903cce7a0606
Jason Fitzpatrick <jason.fitzpatrick@duke.edu>
2023-04-25 22:54:12 -0400

    Implemented color and image form elements

--- src/main/java/oogasalad/view/builder/popupform/ImageParameterStrategy.java
+++ src/main/java/oogasalad/view/builder/popupform/ImageParameterStrategy.java
@@ -5,11 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
-import oogasalad.model.attribute.Attribute;
-import oogasalad.model.attribute.BooleanAttribute;
-import oogasalad.model.attribute.BooleanMetadata;
-import oogasalad.model.attribute.Metadata;
+import oogasalad.model.attribute.*;
 import oogasalad.view.builder.BuilderUtility;
 
 import javax.inject.Inject;
@@ -19,55 +17,55 @@ import java.io.FileNotFoundException;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
-//class ImageParameterStrategy implements ParameterStrategy, BuilderUtility {
-//    private final ImageAttribute attr;
-//    private final ImageMetadata meta;
-//    private Image image;
-//    @Inject
-//    public ImageParameterStrategy(
-//            @Assisted Attribute attr,
-//            @Assisted Metadata meta) {
-//        this.attr = ImageAttribute.from(attr);
-//        this.meta = ImageMetadata.from(meta);
-//    }
-//    @Override
-//    public Node renderInput(ResourceBundle resourceBundle) {
-//        String name = meta.getName();
-//        Node textLabel = new Text(name);
-//        Node element = makeButton("UploadFileTitle", resourceBundle, new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent event) {
-//                Optional<File> file = fileLoad(resourceBundle, "UploadFileTitle");
-//                try {
-//                    image = new Image(new FileInputStream(file.get().getPath()));
-//                } catch (FileNotFoundException e) {
-//                    System.out.println("File for image not found");
-//                }
-//            }
-//        });
-//        return makeHBox(String.format("%sImageInput", name), textLabel, element);
-//    }
-//    @Override
-//    public void saveInput() {
-//        attr.setValue(getFieldValue());
-//    }
-//
-//    @Override
-//    public boolean isInputValid() {
-//        return meta.isValidValue(getFieldValue());
-//    }
-//
-//    @Override
-//    public Metadata getMetadata() {
-//        return meta;
-//    }
-//
-//    @Override
-//    public Attribute getAttribute() {
-//        return attr;
-//    }
-//
-//    private Image getFieldValue() {
-//        return image;
-//    }
-//}
+class ImageParameterStrategy implements ParameterStrategy, BuilderUtility {
+    private final ImageAttribute attr;
+    private final ImageMetadata meta;
+    private Image image;
+    @Inject
+    public ImageParameterStrategy(
+            @Assisted Attribute attr,
+            @Assisted Metadata meta) {
+        this.attr = ImageAttribute.from(attr);
+        this.meta = (ImageMetadata) ImageMetadata.from(meta);
+    }
+    @Override
+    public Node renderInput(ResourceBundle resourceBundle, Pane form) {
+        String name = meta.getName();
+        Node textLabel = new Text(name);
+        Node element = makeButton("UploadFileTitle", resourceBundle, new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Optional<File> file = fileLoad(resourceBundle, "UploadFileTitle");
+                try {
+                    image = new Image(new FileInputStream(file.get().getPath()));
+                } catch (FileNotFoundException e) {
+                    System.out.println("File for image not found");
+                }
+            }
+        });
+        return makeHBox(String.format("%sImageInput", name), textLabel, element);
+    }
+    @Override
+    public void saveInput() {
+        attr.setValue(getFieldValue());
+    }
+
+    @Override
+    public boolean isInputValid() {
+        return meta.isValidValue(getFieldValue());
+    }
+
+    @Override
+    public Metadata getMetadata() {
+        return meta;
+    }
+
+    @Override
+    public Attribute getAttribute() {
+        return attr;
+    }
+
+    private String getFieldValue() {
+        return image.getUrl();
+    }
+}

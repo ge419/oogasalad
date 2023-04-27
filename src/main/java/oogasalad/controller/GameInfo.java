43f2dbbf6fed2c53368e17477d46601214307df4
Changmin Shin <cs521@duke.edu>
2023-04-27 16:36:43 -0400

    added gameinfo

---
+++ src/main/java/oogasalad/controller/GameInfo.java
@@ -0,0 +1,13 @@
+package oogasalad.controller;
+
+public class GameInfo {
+
+  private String title;
+  private String description;
+  private String genre;
+
+  public double width;
+  public double height;
+
+
+}

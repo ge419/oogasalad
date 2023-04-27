94e00317c4311dfe93370e77774bebbab342124b
윤지원 <jiwon.yoon@duke.edu>
2023-04-27 01:28:49 -0400

    Created IntAttribute Tests, Spotted HashCode Error

--- src/test/java/oogasalad/model/attribute/IntAttributeTest.java
+++ src/test/java/oogasalad/model/attribute/IntAttributeTest.java
@@ -1,37 +1,64 @@
 package oogasalad.model.attribute;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class IntAttributeTest {
 
+  private IntAttribute attribute;
+
   @BeforeEach
   void setUp() {
+    attribute = new IntAttribute("testKey", 10);
   }
 
   @Test
-  void from() {
+  void testVariableRetrieval() {
+    assertNotNull(attribute);
+    assertEquals(attribute.getKey(), "testKey");
+    assertEquals(attribute.getValue(), 10);
   }
 
   @Test
-  void getValue() {
+  void testSetValue() {
+    attribute.setValue(20);
+    assertEquals(attribute.getValue(), 20);
   }
 
   @Test
-  void valueProperty() {
+  void testValueProperty() {
+    assertNotNull(attribute.valueProperty());
+    assertEquals(attribute.valueProperty().getValue(), 10);
+
+    attribute.setValue(30);
+    assertEquals(attribute.valueProperty().getValue(), 30);
   }
 
   @Test
-  void setValue() {
+  void testFrom() {
+
   }
 
   @Test
   void testEquals() {
+    IntAttribute sameAttribute = new IntAttribute("testKey", 10);
+    IntAttribute differentAttribute = new IntAttribute("testKey", 20);
+
+    assertTrue(attribute.equals(sameAttribute));
+    assertFalse(attribute.equals(differentAttribute));
   }
 
   @Test
   void testHashCode() {
+    IntAttribute sameAttribute = new IntAttribute("testKey", 10);
+    IntAttribute differentAttribute = new IntAttribute("testKey", 20);
+
+    assertEquals(attribute.hashCode(), sameAttribute.hashCode());
+    assertNotEquals(attribute.hashCode(), differentAttribute.hashCode());
   }
+
 }

355a0f032c7d4c57dac218abbc1f7fcf64f21e94
Trevon Helm <trevon.helm@duke.edu>
2023-04-28 20:18:19 -0400

    I forgot that I commented our width/height/rotation support, so, put that back in!

--- src/main/java/oogasalad/view/builder/rules/BasicRule.java
+++ src/main/java/oogasalad/view/builder/rules/BasicRule.java
@@ -2,20 +2,31 @@ package oogasalad.view.builder.rules;
 
 import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import oogasalad.model.engine.rules.Rule;
 
 /**
+ * <p>Basic implementation of the ViewableRule interface.</p>
+ * <p>This creates a pane with an element on the left (window)
+ * and an element on the right (description).</p>
  *
+ * @author tmh85
  */
 public class BasicRule extends BorderPane implements ViewableRule{
 
   private Rule myRule;
-  private Node myWindow;
-  private Text myDescription;
+  private Pane myWindowPane;
+  private Pane myDescriptionWindow;
+  private BorderPane myPane;
 
   public BasicRule(Rule rule){
     myRule = rule;
+    myPane = new BorderPane();
+    myWindowPane = new Pane();
+    myDescriptionWindow = new Pane();
+    myPane.setLeft(myWindowPane);
+    myPane.setCenter(myDescriptionWindow);
   }
 
   @Override
@@ -25,16 +36,18 @@ public class BasicRule extends BorderPane implements ViewableRule{
 
   @Override
   public void addWindow(String item) {
-
+    myWindowPane.getChildren().clear();
+    myWindowPane.getChildren().add(new Text(item));
   }
 
   @Override
   public void addDescription(String description) {
-
+    myDescriptionWindow.getChildren().clear();
+    myDescriptionWindow.getChildren().add(new Text(description));
   }
 
   @Override
   public Rule getRule() {
-    return null;
+    return myRule;
   }
 }

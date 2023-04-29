f783f9d0232ba5bda83964237ae2ef6bd653e8dc
Aloye Oshotse <ajo24@duke.edu>
2023-04-29 18:52:49 -0400

    fixed the bug where the StackPane was not showing

--- src/main/java/oogasalad/view/gameplay/popup/HandDisplayPopup.java
+++ src/main/java/oogasalad/view/gameplay/popup/HandDisplayPopup.java
@@ -3,25 +3,21 @@ package oogasalad.view.gameplay.popup;
 import java.util.List;
 import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import oogasalad.view.gameplay.pieces.Cards;
-import oogasalad.view.gameplay.pieces.ImageCard;
 import oogasalad.view.tiles.ViewTile;
 
 public class HandDisplayPopup extends BorderPane {
 
-//  private final Cards[] cards;
   private final Stage stage;
 
   public HandDisplayPopup() {
     stage = new Stage();
-//    createScene(this);
   }
 
   public void createScene(Parent root) {
@@ -33,29 +29,24 @@ public class HandDisplayPopup extends BorderPane {
     });
   }
 
-//  public void setSelected(boolean selected) {
-//    // TODO: add a hover feature to each individual card to make it seens as "selected"
-//    for (ImageCard card : cards) {
-//      card.setSelected(selected);
-//    }
-//  }
-
   public void addCards(List<ViewTile> cardList) {
     HBox hand = new HBox();
     for (ViewTile card: cardList) {
       hand.getChildren().add((Node) card);
       hand.setAlignment(Pos.CENTER);
     }
-    Group group = new Group(hand);
-    setCenter(group);
+    ScrollPane scrollPane = new ScrollPane(hand);
+    scrollPane.setFitToWidth(true);
+    scrollPane.setPrefViewportWidth(stage.getWidth());
+    scrollPane.setPrefViewportHeight(stage.getHeight());
+    BorderPane.setAlignment(scrollPane, Pos.CENTER);
+    this.setCenter(scrollPane);
     createScene(this);
   }
 
   public void showHand(Node anchor, Point2D offset) {
-    double sceneWidth = anchor.getScene().getWindow().getWidth();
-    double sceneHeight = anchor.getScene().getWindow().getHeight();
-    double popupX = sceneWidth + offset.getX();
-    double popupY = sceneHeight + offset.getY();
+    double popupX = anchor.getScene().getWindow().getX() + offset.getX();
+    double popupY = anchor.getScene().getWindow().getY() + offset.getY();
     stage.setX(popupX);
     stage.setY(popupY);
     stage.show();

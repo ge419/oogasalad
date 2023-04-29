6290d317228152207f98fb3efca71c21a283c599
Dominic Martinez <dominic.martinez@duke.edu>
2023-04-29 19:17:22 -0400

    use backend tile from factory on frontend tiles

--- src/main/java/oogasalad/view/tiles/BasicTile.java
+++ src/main/java/oogasalad/view/tiles/BasicTile.java
@@ -1,6 +1,7 @@
 package oogasalad.view.tiles;
 
 import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -21,7 +22,7 @@ public class BasicTile extends Rectangle implements ViewTile {
   private final Tile myModelTile;
 
   @Inject
-  public BasicTile(Tile tile) {
+  public BasicTile(@Assisted Tile tile) {
     super(tile.getX(), tile.getY(), tile.getWidth(), tile.getHeight());
     this.myModelTile = tile;
 

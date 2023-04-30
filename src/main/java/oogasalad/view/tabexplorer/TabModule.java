d5bd99d2904fa082f263c9bf20c32f7242494202
Chika Dueke-Eze <chika.duekeeze@duke.edu>
2023-04-29 22:40:14 -0400

    updated tab module to include resource bundle and user preferences class

--- src/main/java/oogasalad/view/tabexplorer/TabModule.java
+++ src/main/java/oogasalad/view/tabexplorer/TabModule.java
@@ -1,21 +1,32 @@
 package oogasalad.view.tabexplorer;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
+import java.util.ResourceBundle;
 import oogasalad.model.accesscontrol.AccessControlModule;
 import oogasalad.view.tabexplorer.tabs.TabFactory;
+import oogasalad.view.tabexplorer.tabs.settings.options.SettingsOptionsFactory;
+import oogasalad.view.tabexplorer.userpreferences.UserPreferences;
 
 /**
- * Guice module for the tab explorer view, which installs the {@link AccessControlModule} and
- * {@link TabFactory}, and defines controller bindings.
- *
- * @author Chika Dueke-Eze
+ * This module defines the dependency injections for the tab explorer view.
+ * It installs the {@link AccessControlModule} and the {@link TabFactory} and defines bindings for
+ * controllers that are used by the tab explorer view.
  */
 public class TabModule extends AbstractModule {
+  private final ResourceBundle resourceBundle;
+
+  public TabModule(ResourceBundle resourceBundle) {
+    this.resourceBundle = resourceBundle;
+  }
   @Override
   protected void configure() {
+    bind(ResourceBundle.class).toInstance(resourceBundle);
+    bind(UserPreferences.class).in(Scopes.SINGLETON);
     install(new AccessControlModule());
     install(new FactoryModuleBuilder().build(TabFactory.class));
-    //todo add binding for controller
+    install(new FactoryModuleBuilder().build(SettingsOptionsFactory.class));
   }
 }
+

5f6f2aca380e1f99fe5dc2627c3ff5b0758c5636
Dominic Martinez <dominic.martinez@duke.edu>
2023-04-30 16:00:09 -0400

    example board image schema/construct

---
+++ src/main/java/oogasalad/model/constructable/BoardImage.java
@@ -0,0 +1,50 @@
+package oogasalad.model.constructable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.inject.Inject;
+import oogasalad.model.attribute.DoubleAttribute;
+import oogasalad.model.attribute.ImageAttribute;
+import oogasalad.model.attribute.PositionAttribute;
+import oogasalad.model.attribute.SchemaDatabase;
+
+public class BoardImage extends AbstractGameConstruct {
+
+  public static final String SCHEMA_NAME = "boardImage";
+  public static final String IMAGE_ATTRIBUTE = "image";
+  public static final String POSITION_ATTRIBUTE = "position";
+  public static final String SCALE_ATTRIBUTE = "scale";
+  public static final String ROTATION_ATTRIBUTE = "rotation";
+
+  @Inject
+  protected BoardImage(SchemaDatabase database) {
+    super(SCHEMA_NAME, database);
+  }
+
+  @JsonIgnore
+  public ImageAttribute imageAttribute() {
+    return ImageAttribute.from(getAttribute(IMAGE_ATTRIBUTE).get());
+  }
+
+  @JsonIgnore
+  public PositionAttribute positionAttribute() {
+    return PositionAttribute.from(getAttribute(POSITION_ATTRIBUTE).get());
+  }
+
+  @JsonIgnore
+  public DoubleAttribute scaleAttribute() {
+    return DoubleAttribute.from(getAttribute(SCALE_ATTRIBUTE).get());
+  }
+
+  @JsonIgnore
+  public DoubleAttribute rotationAttribute() {
+    return DoubleAttribute.from(getAttribute(ROTATION_ATTRIBUTE).get());
+  }
+
+  public String getImage() {
+    return imageAttribute().getValue();
+  }
+
+  public void setImage(String name) {
+    imageAttribute().setValue(name);
+  }
+}

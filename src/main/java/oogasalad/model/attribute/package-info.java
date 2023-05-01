51f65e3af87a0e77bf3bb9c92efb6d3eef988605
Dominic Martinez <dominic.martinez@duke.edu>
2023-05-01 02:58:22 -0400

    add comments

--- src/main/java/oogasalad/model/attribute/package-info.java
+++ src/main/java/oogasalad/model/attribute/package-info.java
@@ -1,11 +1,11 @@
 /**
  * An API for building dynamic, type-safe, serializable data.
  *
- * <p>An {@link oogasalad.model.attribute.AbstractAttribute} is a single key/value pair, with a
+ * <p>An {@link oogasalad.model.attribute.Attribute} is a single key/value pair, with a
  * value
- * defined by the concrete class. {@link oogasalad.model.attribute.AbstractMetadata} describes
+ * defined by the concrete class. {@link oogasalad.model.attribute.Metadata} describes
  * editing information for a field, such as if it can be edited and min/max values.
- * {@link oogasalad.model.attribute.SimpleObjectSchema} defines all the fields a type has and how to
+ * {@link oogasalad.model.attribute.ObjectSchema} defines all the fields a type has and how to
  * edit them, and allows generation of the appropriate attributes.
  */
 package oogasalad.model.attribute;

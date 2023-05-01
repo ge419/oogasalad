85101ef1b71127dc0f2d5a732b5783ca87b0b55e
윤지원 <jiwon.yoon@duke.edu>
2023-05-01 01:24:39 -0400

    Completed Javadoc for Engine Actions

--- src/main/java/oogasalad/model/engine/actions/wins/TileWinningStrategy.java
+++ src/main/java/oogasalad/model/engine/actions/wins/TileWinningStrategy.java
@@ -1,7 +1,17 @@
 package oogasalad.model.engine.actions.wins;
 
 import java.util.List;
+import oogasalad.model.engine.actions.removal.CheckAndRemovePlayerAction;
+import oogasalad.model.engine.actions.removal.PlayerRemovalStrategy;
 
+/**
+ * Concrete implementation of the {@link WinningConditionStrategy}
+ *
+ * Used by Actions to check whether player landed on the winning tile (ex. {@link CheckWinAndEndAction})
+ * Used by games whose board is finite
+ *
+ * @Author Jay Yoon
+ */
 public class TileWinningStrategy implements WinningConditionStrategy {
   private List<String> winningTileIds;
   private String landedTileId;
@@ -11,6 +21,15 @@ public class TileWinningStrategy implements WinningConditionStrategy {
     this.winningTileIds = winningTileIds;
   }
 
+  /**
+   * method that checks whether given tile is one of the winning tiles
+   * <p>
+   *   if the landed tile is contained in the list of winning tiles,
+   *   game end condition is satisfied
+   * </p>
+   *
+   * @return boolean that represents whether winning condition is satisfied
+   */
   @Override
   public boolean isSatisfied() {
     return winningTileIds.contains(landedTileId);

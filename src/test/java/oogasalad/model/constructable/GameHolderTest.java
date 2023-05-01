aa2b3137f17479d8f90b7ea5eee8341f91d12e26
Nathaniel Wullar <nathaniel.wullar@duke.edu>
2023-05-01 01:50:40 -0400

    test for GameHolder

--- src/test/java/oogasalad/model/constructable/GameHolderTest.java
+++ src/test/java/oogasalad/model/constructable/GameHolderTest.java
@@ -2,19 +2,31 @@ package oogasalad.model.constructable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class GameHolderTest {
   GameHolder testGameHolder;
+  Player mockPlayer;
+  Piece mockPiece;
 
   @BeforeEach
   void setup(){
-
+    testGameHolder = new GameHolder();
+    mockPlayer = Mockito.mock(Player.class);
+    mockPiece = Mockito.mock(Piece.class);
+    testGameHolder.setPlayers(new Players());
+    testGameHolder.setCurrentPlayer(mockPlayer);
+    testGameHolder.setPieces(List.of(mockPiece));
   }
 
   @Test
-  void test(){
-
+  void testGetPieceAndPlayerByIdReturnEmptyOptionalSinceTileIsMocked(){
+    assertTrue(testGameHolder.getPieceById("").isEmpty());
+    assertTrue(testGameHolder.getPlayerById("").isEmpty());
+    assertTrue(testGameHolder.getTileById("").isEmpty());
   }
+
 }

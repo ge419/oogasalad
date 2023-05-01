219364e045bf2728c25e503dfce0bae32cfca793
Changmin Shin <cs521@duke.edu>
2023-05-01 01:42:29 -0400

    added tests for GameHolder

--- src/test/java/oogasalad/controller/BuilderControllerTest.java
+++ src/test/java/oogasalad/controller/BuilderControllerTest.java
@@ -2,20 +2,34 @@
 //
 //import static org.junit.jupiter.api.Assertions.*;
 //
+//import oogasalad.model.accesscontrol.dao.GameDao;
 //import oogasalad.model.constructable.GameHolder;
 //import oogasalad.view.BuilderFactory;
+//import oogasalad.view.builder.BuilderView;
 //import oogasalad.view.tabexplorer.userpreferences.Languages;
 //import org.junit.jupiter.api.BeforeAll;
 //import org.junit.jupiter.api.BeforeEach;
 //import org.junit.jupiter.api.Test;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import util.DukeApplicationTest;
 //
-//public class BuilderControllerTest {
+//public class BuilderControllerTest extends DukeApplicationTest {
 //  GameHolder gameHolder;
-//  BuilderController bc;
+//  @Mock
+//  private BuilderView builder;
+//  private BuilderController bc;
+//  @Mock
+//  GameDao dao = Mockito.mock(GameDao.class);
+//  private static final String TEST_LANGUAGE = "en-US";
+//  private static final String TEST_GAME_ID = "DEzmBuvsM4H3zLEhvQa0";
+//
+//  //Languages.ENGLISH.getLocaleStr(),"DEzmBuvsM4H3zLEhvQa0", null)
 //
 //  @BeforeEach
 //  void setup() {
-//    bc = new BuilderController(Languages.ENGLISH.getLocaleStr(),"DEzmBuvsM4H3zLEhvQa0", null);
+//    bc = new BuilderController(TEST_LANGUAGE, TEST_GAME_ID, dao);
+//    builder = bc.getBuilderView();
 //    gameHolder = bc.getGameHolder();
 //  }
 //
--- src/test/java/oogasalad/model/constructable/GameHolderTest.java
+++ src/test/java/oogasalad/model/constructable/GameHolderTest.java
@@ -1,5 +1,77 @@
 package oogasalad.model.constructable;
 
-public class GameHolderTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.guice.ObjectMapperModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.xml.validation.Schema;
+import oogasalad.controller.GameInfo;
+import oogasalad.model.attribute.SchemaDatabase;
+import oogasalad.model.attribute.SimpleSchemaDatabase;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameHolderTest {
+
+  GameHolder gameHolder;
+  ObjectMapper mapper;
+  Injector injector;
+  SchemaDatabase db;
+
+  @BeforeEach
+  void setup() {
+
+
+//    Path path = Paths.get("data", "games", "0hbvOqXKOQdhpgu3aLIO");
+////    gameHolder = mapper.readValue(path.re, GameHolder.class);
+    db = new SimpleSchemaDatabase();
+    mapper =  new ObjectMapper();
+    gameHolder = new GameHolder();
+    injector = Guice.createInjector(new ObjectMapperModule(),
+        binder -> binder.bind(SchemaDatabase.class).toInstance(db));
+  }
+
+  @Test
+  void testDefaultState() {
+    assertNotNull(gameHolder.getPlayers());
+    assertNotNull(gameHolder.getGameInfo());
+    assertNotNull(gameHolder.getBoard());
+    assertNotNull(gameHolder.getRules());
+    assertNotNull(gameHolder.getPieces());
+  }
+
+  @Test
+  void testGetBoard() {
+    Tile tile = new Tile(db);
+    gameHolder.getBoard().addTile(tile);
+    Tile testTile = gameHolder.getBoard().getTiles().get(0);
+    assertEquals(tile, testTile);
+  }
+
+  @Test
+  void testSetBoard() {
+    BBoard board = new BBoard();
+    Tile tile = new Tile(db);
+    board.addTile(tile);
+    gameHolder.setBoard(board);
+    assertEquals(tile, gameHolder.getBoard().getTiles().get(0));
+  }
+
+  @Test
+  void testGameInfo() {
+    GameInfo gameInfo = new GameInfo();
+    gameInfo.setHeight(500);
+    gameInfo.setWidth(500);
+    gameHolder.setGameInfo(gameInfo);
+    assertEquals(gameHolder.getGameInfo().getHeight(), 500);
+    assertEquals(gameHolder.getGameInfo().getHeight(), 500);
+  }
+
 
 }

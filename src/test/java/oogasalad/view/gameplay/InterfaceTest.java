7fa1897fe8c485c11a2c7bce6698a8db8e3b18b4
Woonggyu Jin <wj61@duke.edu>
2023-05-01 01:02:36 -0400

    working on gameview test

--- src/test/java/oogasalad/view/gameplay/InterfaceTest.java
+++ src/test/java/oogasalad/view/gameplay/InterfaceTest.java
@@ -3,6 +3,7 @@ package oogasalad.view.gameplay;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -11,10 +12,12 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import oogasalad.view.Backgroundable;
 import oogasalad.view.Imageable;
 import oogasalad.view.Renderable;
 import oogasalad.view.Textable;
@@ -22,7 +25,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 
-public class InterfaceTest extends ApplicationTest implements Textable, Renderable, Imageable {
+public class InterfaceTest extends ApplicationTest implements Textable, Renderable, Imageable,
+    Backgroundable {
 
   Rectangle testHappyRectangle = new Rectangle();
   Rectangle testSadRectangle = new Rectangle();
@@ -45,10 +49,8 @@ public class InterfaceTest extends ApplicationTest implements Textable, Renderab
     double componentWidth = 200;
     double scale = 2.0;
 
-    // Call the method
     resizeText(text, componentHeight, scale, componentWidth);
 
-    // Verify that the text is properly resized
     assertEquals(componentWidth / WRAPPING_BUFFER, text.getWrappingWidth(), 0.01);
     assertEquals(TextAlignment.CENTER, text.getTextAlignment());
     assertNotEquals(1.0, text.getScaleX());
@@ -76,12 +78,14 @@ public class InterfaceTest extends ApplicationTest implements Textable, Renderab
   public void testCreateImage() {
     double width = 200;
     String imgPath = "view.gameplay/default.jpg";
+    String sadPath = "not/a/path";
     double imgScale = 2.0;
 
-    // Call the method
     ImageView imageView = createImage(width, imgPath, imgScale);
 
-    // Verify that the ImageView is properly created
+    //sad case:
+    assertThrows(IllegalArgumentException.class, () -> createImage(width, sadPath, imgScale));
+
     assertNotNull(imageView);
     Image img = imageView.getImage();
     assertNotNull(img);
@@ -90,6 +94,23 @@ public class InterfaceTest extends ApplicationTest implements Textable, Renderab
     assertEquals(width / imgScale, imageView.getFitWidth(), 0.01);
     assertTrue(imageView.isSmooth());
     assertTrue(imageView.isCache());
+  }
+
+  @Test
+  public void testCreateBackground() {
+    double width = 200;
+    double height = 100;
+    Color backgroundColor = Color.LIGHTBLUE;
+    Color strokeColor = Color.BLACK;
+
+    Rectangle background = createBackground(width, height, backgroundColor, strokeColor);
+
+    assertNotNull(background);
+    assertEquals(width, background.getWidth(), 0.01);
+    assertEquals(height, background.getHeight(), 0.01);
+    assertEquals(backgroundColor, background.getFill());
+    assertEquals(strokeColor, background.getStroke());
+    assertEquals(STROKE_WIDTH, background.getStrokeWidth(), 0.01);
   }
 
   @Override

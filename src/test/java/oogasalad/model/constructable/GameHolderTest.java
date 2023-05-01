27f7a1cee7604a57f10581d468c2a4ae94a7c1c8
Changmin Shin <cs521@duke.edu>
2023-05-01 02:10:38 -0400

    added gameinfotest, more tests in gameholdertest

--- src/test/java/oogasalad/model/constructable/GameHolderTest.java
+++ src/test/java/oogasalad/model/constructable/GameHolderTest.java
@@ -9,6 +9,8 @@ import com.google.inject.Injector;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.validation.Schema;
 import oogasalad.controller.GameInfo;
 import oogasalad.model.attribute.SchemaDatabase;
@@ -26,10 +28,6 @@ class GameHolderTest {
 
   @BeforeEach
   void setup() {
-
-
-//    Path path = Paths.get("data", "games", "0hbvOqXKOQdhpgu3aLIO");
-////    gameHolder = mapper.readValue(path.re, GameHolder.class);
     db = new SimpleSchemaDatabase();
     mapper =  new ObjectMapper();
     gameHolder = new GameHolder();
@@ -73,5 +71,69 @@ class GameHolderTest {
     assertEquals(gameHolder.getGameInfo().getHeight(), 500);
   }
 
+  @Test
+  void testPlayers() {
+    List<Player> playerList = new ArrayList<>();
+    Player player = new Player(db);
+    playerList.add(player);
+    Players players = new Players();
+    players.setPlayers(playerList);
+    gameHolder.setPlayers(players);
+    assertEquals(player, gameHolder.getPlayers().getList().get(0));
 
+  }
+
+  @Test
+  void testCurrentPlayer() {
+    List<Player> playerList = new ArrayList<>();
+    Player player1 = new Player(db);
+    Player player2 = new Player(db);
+    playerList.add(player1);
+    playerList.add(player2);
+    Players players = new Players(playerList);
+    gameHolder.setPlayers(players);
+    gameHolder.setCurrentPlayer(player2);
+    assertEquals(player2, gameHolder.getCurrentPlayer());
+
+    gameHolder.setCurrentPlayer(player1);
+    assertEquals(player1, gameHolder.getCurrentPlayer());
+  }
+
+  @Test
+  void testRemovePlayers() {
+    List<Player> playerList = new ArrayList<>();
+    Player player1 = new Player(db);
+    Player player2 = new Player(db);
+    playerList.add(player1);
+    playerList.add(player2);
+    Players players = new Players(playerList);
+    gameHolder.setPlayers(players);
+    gameHolder.removePlayers(playerList);
+    assertTrue(gameHolder.getPlayers().getList().isEmpty());
+  }
+
+  @Test
+  void testPieces() {
+    Piece piece = new Piece(db);
+    List<Piece> pieceList = new ArrayList<>();
+    pieceList.add(piece);
+
+    gameHolder.setPieces(pieceList);
+    assertEquals(piece, gameHolder.getPieces().get(0));
+  }
+
+  @Test
+  void testTiles() {
+    Tile tile1 = new Tile(db);
+    Tile tile2 = new Tile(db);
+    Tile tile3 = new Tile(db);
+    BBoard board = new BBoard();
+    board.addTile(tile1);
+    board.addTile(tile2);
+    board.addTile(tile3);
+    gameHolder.setBoard(board);
+    String id1 = gameHolder.getBoard().getTiles().get(0).getId();
+    assertEquals(gameHolder.getBoard().getTileCount(), 3);
+    assertTrue(gameHolder.getTileById(id1).isPresent());
+  }
 }
